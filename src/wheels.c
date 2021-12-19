6117ffbafbbd86628be54e20206e0704d4d5e3fb
Anton Hlinisty <anton.hlinisty@gmail.com>
2021-12-19 14:32:32 +0300

    Cast pin numbers to char arrays

--- src/wheels.c
+++ src/wheels.c
@@ -2,10 +2,10 @@
 
 #include "filewriter.h"
 
-#define RIGHT_FORWARD_PIN 22  //BCM pin number, corresponds 15 physical pin number
-#define RIGHT_BACKWARD_PIN 23 //BCM pin number, corresponds 16 physical pin number
-#define LEFT_FORWARD_PIN 26   //BCM pin number, corresponds 37 physical pin number
-#define LEFT_BACKWARD_PIN 12  //BCM pin number, corresponds 32 physical pin number
+#define RIGHT_FORWARD_PIN "22"  //BCM pin number, corresponds 15 physical pin number
+#define RIGHT_BACKWARD_PIN "23" //BCM pin number, corresponds 16 physical pin number
+#define LEFT_FORWARD_PIN "26"   //BCM pin number, corresponds 37 physical pin number
+#define LEFT_BACKWARD_PIN "12"  //BCM pin number, corresponds 32 physical pin number
 
 #define OUT_DIRECTION "out"
 
